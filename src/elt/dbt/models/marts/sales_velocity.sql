a1c918fdec146c3fefcfa8dc7e342e4a9507dc4c
Thanh Vu <thanhauco@gmail.com>
2023-09-21 00:00:00 -0700

    ELT: dbt Core Project Structure

---
+++ src/elt/dbt/models/marts/sales_velocity.sql
@@ -0,0 +1,25 @@
+{{ config(
+    materialized='table',
+    tags=['finance', 'core'],
+    unique_key='id'
+) }}
+
+with source as (
+    select * from {{ ref('stg_sales_velocity') }}
+),
+
+transformed as (
+    select
+        id,
+        user_id,
+        amount * 100 as amount_cents,
+        status,
+        case 
+            when status = 'paid' then amount 
+            else 0 
+        end as recognized_revenue,
+        created_at
+    from source
+)
+
+select * from transformed
