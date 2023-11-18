6529c58bfc7ee8a0c5d80d78b67479df602c6556
Thanh Vu <thanhauco@gmail.com>
2023-11-18 00:00:00 -0800

    AI: Business Prompt Library

---
+++ src/genai/templates/financial_forecast.py
@@ -0,0 +1,32 @@
+# Prompt Template for: Financial Forecast
+
+SYSTEM_PROMPT = """
+You are an expert AI assistant specialized in financial forecast.
+Your goal is to provide accurate, professional, and actionable output.
+Follow these constraints:
+1. Be concise.
+2. Use markdown formatting.
+3. Cite sources if applicable.
+"""
+
+USER_TEMPLATE = """
+Task: financial forecast
+Context:
+{context}
+
+History:
+{history}
+
+Instructions:
+Please generate a comprehensive response addressing the specific needs outlined in the context.
+"""
+
+FEW_SHOT_EXAMPLES = [
+    {
+        "input": "Example Context 1",
+        "output": "Example Output 1 reflecting high quality."
+    }
+]
+
+def format_prompt(context, history=[]):
+    return f"{SYSTEM_PROMPT}\n\n{USER_TEMPLATE.format(context=context, history=history)}"
---
+++ src/genai/templates/paraphrasing.py
@@ -0,0 +1,32 @@
+# Prompt Template for: Paraphrasing
+
+SYSTEM_PROMPT = """
+You are an expert AI assistant specialized in paraphrasing.
+Your goal is to provide accurate, professional, and actionable output.
+Follow these constraints:
+1. Be concise.
+2. Use markdown formatting.
+3. Cite sources if applicable.
+"""
+
+USER_TEMPLATE = """
+Task: paraphrasing
+Context:
+{context}
+
+History:
+{history}
+
+Instructions:
+Please generate a comprehensive response addressing the specific needs outlined in the context.
+"""
+
+FEW_SHOT_EXAMPLES = [
+    {
+        "input": "Example Context 1",
+        "output": "Example Output 1 reflecting high quality."
+    }
+]
+
+def format_prompt(context, history=[]):
+    return f"{SYSTEM_PROMPT}\n\n{USER_TEMPLATE.format(context=context, history=history)}"
